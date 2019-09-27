aefc3f970938afc5ad612498da880805494cb0af
Alex J <jones6211@gmail.com>
2019-09-27 16:41:49 -0500

    update images and style

--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -2,32 +2,33 @@ const INITIAL_STATE = {
   sections: [
     {
       title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+      imageUrl: "https://i.ibb.co/T1MtkP0/hat.jpg",
       id: 1,
       linkUrl: "shop/hats"
     },
     {
       title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+      imageUrl:
+        "https://i.ibb.co/rb33D8g/2027778-different-color-leather-jacket-hanging-on-rack.jpg",
       id: 2,
       linkUrl: "shop/jackets"
     },
     {
       title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+      imageUrl: "https://i.ibb.co/6ZgMyn7/Best-Sneaker-Affiliate-Programs.jpg",
       id: 3,
       linkUrl: "shop/sneakers"
     },
     {
       title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+      imageUrl: "https://i.ibb.co/vY2JTvD/photo-1485811661309-ab85183a729c.jpg",
       size: "large",
       id: 4,
       linkUrl: "shop/womens"
     },
     {
       title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+      imageUrl: "https://i.ibb.co/V3DB8GY/photo-1514222709107-a180c68d72b4.jpg",
       size: "large",
       id: 5,
       linkUrl: "shop/mens"
